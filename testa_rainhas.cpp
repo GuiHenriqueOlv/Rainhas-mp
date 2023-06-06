8584554e30601586eabd17dfe57989ce3e4524d1
Guilherme Henrique Oliveira Araujo <guilhermeoliveira301@hotmail.com>
2023-06-06 19:02:43 -0300

    teste de mais de uma rainha na mesma diagonal principal

--- testa_rainhas.cpp
+++ testa_rainhas.cpp
@@ -136,3 +136,18 @@ TEST_CASE("Teste de validação do tabuleiro inválido: mais de uma rainha na me
 
     REQUIRE(verificaSolucao(board) == 0);
 };
+
+TEST_CASE("Teste de validação do tabuleiro inválido: mais de uma rainha na mesma diagonal principal") {
+    std::vector<int> board = {
+        1, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 1, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 1,
+        0, 0, 0, 0, 0, 1, 0, 0,
+        0, 0, 1, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 1, 0,
+        0, 1, 0, 0, 0, 0, 0, 0,
+        0, 0, 1, 0, 0, 0, 0, 0
+    };
+
+    REQUIRE(verificaSolucao(board) == 0);
+};
