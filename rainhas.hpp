cccc8b32f4361f4c40fc7f10f07cdbbc67367726
Guilherme Henrique Oliveira Araujo <guilhermeoliveira301@hotmail.com>
2023-06-06 23:22:46 -0300

    Projeto atualizado, com o cpplint correto agora

--- rainhas.hpp
+++ rainhas.hpp
@@ -19,9 +19,9 @@
 /// Esta função verifica se o vetor de entrada contém posições válidas para as rainhas no problema das N-Rainhas.
 /// O vetor deve representar uma permutação dos números de 0 a N-1, onde cada número representa a posição da linha
 /// da rainha na coluna correspondente.
-///Assertivas de entrada:
+/// Assertivas de entrada:
 /// \param vetorTab O vetor de entrada que representa as posições das rainhas.
-///Assertivas de saída:
+/// Assertivas de saída:
 /// \return 0 se o vetor for válido, -1 caso contrário.
 int validaVetor(const std::vector<int>& vetorTab);
 
@@ -29,9 +29,9 @@ int validaVetor(const std::vector<int>& vetorTab);
 ///
 /// Esta função verifica se as rainhas no tabuleiro se ameaçam em linhas e colunas.
 /// Ela verifica se há algum conflito na mesma linha ou coluna.
-///Assertivas de entrada:
+/// Assertivas de entrada:
 /// \param vetorTab O vetor que representa as posições das rainhas.
-///Assertivas de saída:
+/// Assertivas de saída:
 /// \return 0 se nenhum conflito for encontrado, -1 caso contrário.
 int verificaLinhaEColuna(const std::vector<int>& vetorTab);
 
@@ -39,9 +39,9 @@ int verificaLinhaEColuna(const std::vector<int>& vetorTab);
 ///
 /// Esta função verifica se as rainhas no tabuleiro se ameaçam em diagonais.
 /// Ela verifica se há algum conflito nas diagonais.
-///Assertivas de entrada:
+/// Assertivas de entrada:
 /// \param vetorTab O vetor que representa as posições das rainhas.
-///Assertivas de saída:
+/// Assertivas de saída:
 /// \return 0 se nenhum conflito for encontrado, -1 caso contrário.
 int verificaDiagonais(const std::vector<int>& vetorTab);
 
@@ -49,9 +49,9 @@ int verificaDiagonais(const std::vector<int>& vetorTab);
 ///
 /// Esta função verifica se o vetor de entrada representa uma solução válida para o problema das N-Rainhas.
 /// Ela valida o vetor verificando se não há conflitos em linhas, colunas e diagonais.
-///Assertivas de entrada:
+/// Assertivas de entrada:
 /// \param vetorTab O vetor de entrada que representa as posições das rainhas.
-///Assertivas de saída:
+/// Assertivas de saída:
 /// \return 0 se o vetor representa uma solução válida, -1 caso contrário.
 int verificaSolucao(const std::vector<int>& vetorTab);
 
